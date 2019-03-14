2bf7889e7cd546703348a88fd27e298c10533e85
mayankshah1607 <mayankshah1614@gmail.com>
2019-03-14 17:12:41 +0530

    Structured server.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const port = process.env.PORT || 2000;
+const cookieParser = require('cookie-parser');
+
+require('dotenv').config();
+const app = express()
+
+app.all('/*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With,     Content-Type");
+    next();
+});
+
+mongoose.connect(process.env.MONGO_DB_URL, (err) => {
+    err ? console.log("Error connecting to database!") : console.log("Successfully connected to the database!");
+});
+
+app.use('/auth',require('./Routes/auth'))
+app.use('/auth',require('./Routes/question'))
+app.listen(port, () => {
+    console.log(`App started on port: ${port}.`);
+});
